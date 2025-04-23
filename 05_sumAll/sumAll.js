a24438cd1b406663d9cf45f5996795747f68cfd7
drdexe <104161856+drdexe@users.noreply.github.com>
2025-04-23 16:24:10 +0800

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (
+    !Number.isInteger(a) || !Number.isInteger(b) ||
+    a <= 0 || b <= 0
+  ) return 'ERROR';
 
+  const start = Math.min(a, b);
+  const end = Math.max(a, b);
+
+  let sum = 0;
+  for (let num = start; num <= end; num++) {
+    sum += num;
+  }
+  return sum;
 };
 
 // Do not edit below this line
