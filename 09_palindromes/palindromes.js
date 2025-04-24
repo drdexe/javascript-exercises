7379b82db49fe00ae1c5c2eb08637eadbdba1858
drdexe <104161856+drdexe@users.noreply.github.com>
2025-04-24 23:52:44 +0800

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function(string) {
+  const lowerCaseString = string.toLowerCase();
 
+  let alphanumericString = "";
+  for (let i = 0; i < string.length; i++) {
+    const code = lowerCaseString.charCodeAt(i);
+    if (
+      code >= 48 && code <= 57 ||  // 0-9
+      code >= 97 && code <= 122  // a-z
+    ) {
+      alphanumericString += lowerCaseString[i];
+    }
+  }
+  
+  return alphanumericString === alphanumericString.split("").reverse().join("");
 };
 
 // Do not edit below this line
