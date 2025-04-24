681686ab9c1fc0f462af901983ceed2c3630a571
drdexe <104161856+drdexe@users.noreply.github.com>
2025-04-25 00:32:37 +0800

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.toSorted((a, b) => {
+    const ages = [a, b].map(person => {
+      if ("yearOfDeath" in person) {
+        return person.yearOfDeath - person.yearOfBirth;
+      } else {
+        return new Date().getFullYear() - person.yearOfBirth;
+      }
+    });
+    return ages[1] - ages[0];
+  })[0];
 };
 
 // Do not edit below this line
